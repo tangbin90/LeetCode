98495702be23aa187b0bdf9315dee70d0992ef1b
tangbin90 <tangbin90@gmail.com>
2017-07-05 23:23:24 +0800

    LeetCode
    problems KGroup

---
+++ src/ListNode.java
@@ -0,0 +1,11 @@
+/**
+ * @author TangBin
+ * @version V1.0
+ * @date 04/07/2017 10:05 PM
+ */
+
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
