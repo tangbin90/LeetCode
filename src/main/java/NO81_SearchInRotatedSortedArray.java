0a16f0b264ce4cb16f01582d3aad5afe3f33acbd
BinTang <tangbin90@users.noreply.github.com>
2017-12-07 17:02:51 +0800

    NO80 NO81

---
+++ src/main/java/NO81_SearchInRotatedSortedArray.java
@@ -0,0 +1,19 @@
+/**
+ * @author: 17082720 tangbin
+ * @create: 2017/12/7 17:02
+ * @description:
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Write a function to determine if a given target is in the array.
+
+The array may contain duplicates.
+
+
+ */
+public class NO81_SearchInRotatedSortedArray {
+    public boolean search(int[] nums, int target) {
+        return true;
+    }
+}
