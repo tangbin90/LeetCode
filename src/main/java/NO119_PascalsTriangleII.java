db47f16f50c69bb835fb7162a3b00a5efda37fb2
BinTang <tangbin90@users.noreply.github.com>
2018-02-24 14:39:13 +0800

    NO119 NO120

---
+++ src/main/java/NO119_PascalsTriangleII.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C), 2015-2018, 苏宁金融集团
+ * Author: TangBin 17082720
+ * FileName: NO119_PascalsTriangleII
+ * Date: 2018/2/24 9:45
+ * Description:
+ */
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author: TangBin 17082720 
+ * @Email: 17082720@cnsuning.com
+ * @create: 2018/2/24 9:45
+ * @since 1.0.0
+ * @description: 〈〉
+ */
+public class NO119_PascalsTriangleII {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> li = new ArrayList<>(rowIndex+1);
+        if(rowIndex<0)
+            return li;
+        li.add(1);
+        for(int i=1;i<rowIndex+1;i++){
+            li.add(0);
+            for(int j=i;j>0;j--){
+                li.set(j,li.get(j)+li.get(j-1));
+            }
+        }
+        return li;
+    }
+
+    public static void main(String[] args) {
+        NO119_PascalsTriangleII pascal = new NO119_PascalsTriangleII();
+        List<Integer> aa = pascal.getRow(10);
+        System.out.println(aa.toString());
+    }
+}
---
+++ src/main/java/NO120_Triangle.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C), 2015-2018, 苏宁金融集团
+ * Author: TangBin 17082720
+ * FileName: NO120_Triangle
+ * Date: 2018/2/24 11:05
+ * Description:
+ */
+
+import javafx.beans.property.ReadOnlyBooleanWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: TangBin 17082720 
+ * @Email: 17082720@cnsuning.com
+ * @create: 2018/2/24 11:05
+ * @since 1.0.0
+ * @description: 〈〉
+ */
+public class NO120_Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int rowNum = triangle.size();
+        int[] calculatedNum = new int[rowNum];
+        for(int i=0;i<rowNum;i++)
+            calculatedNum[i]=0;
+        boolean[] calculated = new boolean[rowNum];
+        return minimumTotalHelper(0,0,triangle);
+    }
+
+    public int minimumTotal2(List<List<Integer>> triangle){
+        List<Integer> calculatedNum = triangle.get(triangle.size()-1);
+        for(int i=triangle.size()-2;i>=0;i--){
+            List<Integer> nums = triangle.get(i);
+            for(int j=0;j<nums.size();j++){
+                calculatedNum.set(j,Math.min(calculatedNum.get(j),calculatedNum.get(j+1))+nums.get(j));
+            }
+        }
+        return calculatedNum.get(0);
+    }
+
+    private int minimumTotalHelper(int row,int col, List<List<Integer>> triangle){
+        int rowNum = triangle.size();
+        if(row==rowNum-1)
+            return triangle.get(row).get(col);
+
+        int num =  triangle.get(row).get(col);
+        int minLeft = minimumTotalHelper(row+1,col,triangle);
+        int minRight = minimumTotalHelper(row+1,col+1,triangle);
+        num = Math.min(minLeft,minRight)+num;
+        return num;
+    }
+
+    public static void main(String[] args){
+        List<List<Integer>> ll = new ArrayList<>();
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(1);
+        ll.add(l1);
+
+        List<Integer> l2 = new ArrayList<>();
+        l2.add(-5);
+        l2.add(-2);
+        ll.add(l2);
+
+        List<Integer> l3 = new ArrayList<>();
+        l3.add(3);
+        l3.add(6);
+        l3.add(1);
+        ll.add(l3);
+
+        List<Integer> l4 = new ArrayList<>();
+        l4.add(-1);
+        l4.add(2);
+        l4.add(4);
+        l4.add(-3);
+        ll.add(l4);
+
+        NO120_Triangle triangle = new NO120_Triangle();
+        System.out.println( triangle.minimumTotal2(ll));
+
+    }
+}
