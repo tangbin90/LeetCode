d63d9cc4063dd0bd8fbbefbcf01dad8144e8ac30
tangbin90 <tangbin90@gmail.com>
2018-03-21 19:04:48 +0800

    NO89 NO92 NO93

---
+++ src/main/java/NO89_GrayCode.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author TangBin
+ * @version V1.0
+ * @date 20/03/2018 10:23 AM
+ */
+public class NO89_GrayCode {
+    public List<Integer> grayCode(int n) {
+        List<Integer> li = new LinkedList<>();
+        li.add(0);
+        if(n==0)
+            return li;
+        for(int i=0;i<n;i++){
+            int length = li.size();
+            for (int j = length-1; j >=0 ; j--)
+                li.add(li.get(j) + (1 << i));
+        }
+        return li;
+    }
+
+    public static void main(String[] args){
+        NO89_GrayCode grayCode = new NO89_GrayCode();
+        List<Integer> li = grayCode.grayCode(3);
+        System.out.println(li.toString());
+    }
+}
---
+++ src/main/java/NO92_ReverseLinkedList.java
@@ -0,0 +1,64 @@
+/**
+ * @author TangBin
+ * @version V1.0
+ * @date 20/03/2018 6:37 PM
+ * Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list.
+
+
+ */
+public class NO92_ReverseLinkedList {
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if(m==n)
+            return head;
+        int count = 1;
+
+        ListNode node = head;
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = node;
+        ListNode preNode = fakeHead;
+        ListNode startNode = head;
+        while(node!=null){
+            if(count==m){
+                startNode = preNode;
+            }
+            if(count==n)
+                break;
+            count++;
+            preNode = node;
+            node = node.next;
+        }
+
+
+        ListNode last = node.next;
+        ListNode first = startNode.next;
+        ListNode tempFirst = first;
+        ListNode subPreNode = new ListNode(0);
+        subPreNode.next = first;
+        while(first!=last){
+            ListNode temp = first.next;
+            first.next = subPreNode;
+            subPreNode = first;
+            first = temp;
+        }
+        startNode.next = node;
+        tempFirst.next = last;
+        return fakeHead.next;
+    }
+
+    public static void main(String[] args) {
+        NO92_ReverseLinkedList reverseLinkedList = new NO92_ReverseLinkedList();
+        ListNode node = new ListNode(3);
+        ListNode next  = new ListNode(5);
+        node.next = next;
+        reverseLinkedList.reverseBetween(node,1,2);
+    }
+}
