b3f343015f1f8b99d0dd697497da1642203b7b25
tangbin90 <tangbin90@gmail.com>
2018-03-26 16:30:58 +0800

    NO115 NO125

---
+++ src/main/java/entity/TreeLinkNode.java
@@ -0,0 +1,12 @@
+package entity;
+
+/**
+ * @author TangBin
+ * @version V1.0
+ * @date 26/03/2018 3:05 PM
+ */
+public class TreeLinkNode {
+      int val;
+      public TreeLinkNode left, right, next;
+      TreeLinkNode(int x) { val = x; }
+}
