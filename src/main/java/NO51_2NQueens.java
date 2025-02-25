09ce577b4eebbd4e6ed4c05d6d11a032f5c091b5
BinTang <tangbin90@users.noreply.github.com>
2025-02-26 00:47:37 +0800

    Refactor NO51_2NQueens: Improve N-Queens solution implementation

--- src/main/java/NO51_2NQueens.java
+++ src/main/java/NO51_2NQueens.java
@@ -4,54 +4,70 @@ import java.util.List;
 
 public class NO51_2NQueens {
 
-    List<List<char[]>> res = new LinkedList<>();
 
-    public List<List<String>> solveNQueens(int n) {
+    public List<Character[][]> solveNQueens(int n) {
         //Initialize the board
+        List<Character[][]> res = new LinkedList<>();
 
-        List<char[]> board = new LinkedList<>();
+        Character[][] board = new Character[n][n];
         for(int i = 0; i< n; i++){
-            char[] row = new char[n];
-            Arrays.fill(row, '.');
-            board.add(row);
+            Arrays.fill(board[i], '.');
         }
 
         //DFS
-        dfs(board, 0, n);
+        dfs(board, 0, n, res);
         return res;
     }
 
-    void dfs(List<char[]> board, int row, int n){
+    void dfs(Character[][] board, int row, int n, List<Character[][]> res){
         if(row == n){
-            res.add(board);
+            Character[][] copy = new Character[n][n];
+            for(int i = 0; i<n; i++){
+                copy[i] = board[i].clone();
+            }
+            res.add(copy);
             return;
         }
 
         for(int i = 0; i<n; i++){
             if(isValid(board, row, i)){
-                board.get(row).re;
-                dfs(board, row+1, n);
-                board.get(row).set(i, ".");
+                board[row][i] = 'Q';
+                dfs(board, row+1, n, res);
+                board[row][i] = '.';
             }
         }
     }
-    public boolean isValid(List<char[]> board, int row, int col){
-        int n = board.size();
+    public boolean isValid(Character[][] board, int row, int col){
+        int n = board.length;
         for(int i = 0; i<row; i++){
-            if(board.get(i)[col] == 'Q')
+            if(board[i][col] == 'Q')
                 return false;
         }
 
         for(int i = row-1, j = col-1; i>=0&&j>=0; i--, j--){
-            if(board.get(i)[j] == 'Q')
+            if(board[i][j] == 'Q')
                 return false;
         }
 
         for(int i = row-1, j = col+1; i>=0&&j<n; i--, j++){
-            if(board.get(i)[j] == 'Q')
+            if(board[i][j] == 'Q')
                 return false;
         }
         return true;
 
     }
+
+    public static void main(String[] args) {
+        NO51_2NQueens solution = new NO51_2NQueens();
+        List<Character[][]> res = solution.solveNQueens(4);
+        for(Character[][] board : res){
+            for(Character[] row : board){
+                for(Character c : row) {
+                    System.out.print(c);
+                }
+                System.out.println();       
+            }
+            System.out.println();
+        }
+    }   
 }
