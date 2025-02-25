ce02cfc5840b42ad20229280ce49a0393cef6a93
BinTang <tangbin90@users.noreply.github.com>
2025-02-26 00:54:29 +0800

    Optimize N-Queens solution: Convert to primitive char array and improve code readability

--- src/main/java/NO51_2NQueens.java
+++ src/main/java/NO51_2NQueens.java
@@ -1,71 +1,70 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class NO51_2NQueens {
 
+    public List<char[][]> solveNQueens(int n) {
+        // Initialize the board
+        List<char[][]> res = new ArrayList<>();
 
-    public List<Character[][]> solveNQueens(int n) {
-        //Initialize the board
-        List<Character[][]> res = new LinkedList<>();
-
-        Character[][] board = new Character[n][n];
-        for(int i = 0; i< n; i++){
+        char[][] board = new char[n][n];
+        for(int i = 0; i < n; i++) {
             Arrays.fill(board[i], '.');
         }
 
-        //DFS
+        // DFS
         dfs(board, 0, n, res);
         return res;
     }
 
-    void dfs(Character[][] board, int row, int n, List<Character[][]> res){
-        if(row == n){
-            Character[][] copy = new Character[n][n];
-            for(int i = 0; i<n; i++){
+    void dfs(char[][] board, int row, int n, List<char[][]> res) {
+        if(row == n) {
+            char[][] copy = new char[n][n];
+            for(int i = 0; i < n; i++) {
                 copy[i] = board[i].clone();
             }
             res.add(copy);
             return;
         }
 
-        for(int i = 0; i<n; i++){
-            if(isValid(board, row, i)){
+        for(int i = 0; i < n; i++) {
+            if(isValid(board, row, i)) {
                 board[row][i] = 'Q';
-                dfs(board, row+1, n, res);
+                dfs(board, row + 1, n, res);
                 board[row][i] = '.';
             }
         }
     }
-    public boolean isValid(Character[][] board, int row, int col){
-        int n = board.length;
-        for(int i = 0; i<row; i++){
+
+    public boolean isValid(char[][] board, int row, int col) {
+        // Check column
+        for(int i = 0; i < row; i++) {
             if(board[i][col] == 'Q')
                 return false;
         }
 
-        for(int i = row-1, j = col-1; i>=0&&j>=0; i--, j--){
+        // Check diagonal \
+        for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
             if(board[i][j] == 'Q')
                 return false;
         }
 
-        for(int i = row-1, j = col+1; i>=0&&j<n; i--, j++){
+        // Check diagonal /
+        for(int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
             if(board[i][j] == 'Q')
                 return false;
         }
+        
         return true;
-
     }
 
     public static void main(String[] args) {
         NO51_2NQueens solution = new NO51_2NQueens();
-        List<Character[][]> res = solution.solveNQueens(4);
-        for(Character[][] board : res){
-            for(Character[] row : board){
-                for(Character c : row) {
-                    System.out.print(c);
-                }
-                System.out.println();       
+        List<char[][]> res = solution.solveNQueens(4);
+        for(char[][] board : res) {
+            for(char[] row : board) {
+                System.out.println(new String(row));
             }
             System.out.println();
         }
