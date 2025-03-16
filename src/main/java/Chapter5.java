f1cd275c5641465e997f3a96395b48f05c16fe4c
BinTang <tangbin90@users.noreply.github.com>
2025-03-16 13:08:58 +0800

    Add new alg

--- src/main/java/Chapter5.java
+++ src/main/java/Chapter5.java
@@ -1,7 +1,6 @@
+
 import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.Stack;
+import java.util.*;
 
 public class Chapter5 {
     public static int countPrimes(int n){
@@ -137,10 +136,146 @@ public class Chapter5 {
         return false;
     }
 
-    public static void main(String[] args) {
-        int[] piles = {2,34,5,1};
+    // water[i] = min(left_max, right_max) - height[i]
 
-        System.out.println(minEatingSpeed(piles, 10));
+    public static int trap(int[] height){
+        int n = height.length;
+        int ans = 0;
+
+        for(int i=1; i<n-1; i++){
+            int l_max = 0, r_max = 0;
+            for(int j=i; j >= 0; j--){
+                l_max = Math.max(height[j], l_max);
+            }
+
+            for(int j=i;j < n; j++){
+                r_max = Math.max(r_max, height[j]);
+            }
+            ans += Math.min(l_max, r_max) - height[i];
+        }
+        return ans;
+    }
+
+    public static int trap2(int[] height){
+        if(height.length == 0)
+            return 0;
+
+        int n = height.length;
+        int ans = 0;
+        int[] lMax = new int[height.length];
+        int[] rMax = new int[height.length];
+
+        int ltmp = 0;
+        int rtmp = 0;
+        for(int i=0; i<height.length; i++){
+            if(height[i] > ltmp){
+                ltmp = height[i];
+            }
+            lMax[i] = ltmp;
+        }
+
+        for(int i=n-1; i>=0; i--){
+            if(height[i] > rtmp){
+                rtmp = height[i];
+            }
+            rMax[i] = rtmp;
+        }
+
+
+        for(int i=1; i<n-1; i++){
+            ans += Math.min(lMax[i], rMax[i]) - height[i];
+        }
+
+        return ans;
+    }
+
+    public static int trap3(int[] height){
+        if(height.length == 0)
+            return 0;
+
+        int lMax = height[0];
+        int rMax = height[height.length-1];
+        int right = height.length -2;
+        int left = 1;
+        int ans = 0;
+        while(left <= right){
+            lMax = Math.max(lMax, height[left]);
+            rMax = Math.max(rMax, height[right]);
+
+            if(lMax <= rMax){
+                ans += lMax - height[left];
+                left++;
+            } else {
+                ans += rMax - height[right];
+                right--;
+            }
+        }
+        return ans;
+    }
+
+    public static int removeDuplicates(int[] nums){
+        int n = nums.length;
+        if(n == 0) return 0;
+        int slow = 0;
+        int fast = 1;
+
+        while(fast < nums.length){
+            if(nums[slow]!=nums[fast]){
+                slow++;
+                nums[slow] = nums[fast];
+            }
+            fast++;
+        }
+        return slow+1;
+    }
+
+    public static String palindrome(String str, int l , int r){
+        while(l>=0 && r < str.length()){
+            if(str.charAt(l)==str.charAt(r)){
+                l--;
+                r++;
+            } else
+                break;
+        }
+
+        return str.substring(l+1, r-l-1);
+    }
+
+    public static String longestPalindrome(String str){
+        String res ="";
+
+        for(int i=0; i<str.length(); i++){
+            String s1 = palindrome(str, i, i);
+            String s2 = palindrome(str, i, i+1);
+            res = s1.length()>res.length()? s1 : res;
+            res = s2.length()>res.length()? s2 : res;
+        }
+
+        return res;
+    }
+
+    //dp[i]-> di[i+1...dp[i]]
+    public static boolean canJump(int[] nums, int index, Map<Integer, Boolean> mem){
+        if(index >= nums.length-1)
+            return true;
+        for(int i=1; i<= nums[nums[index]]; i++){
+            if(mem.containsKey(index+i))
+                return mem.get(index+i);
+            else {
+                boolean rslt = canJump(nums, index + i, mem);
+                mem.put(index, rslt);
+                return rslt;
+            }
+        }
+
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        int[] piles = {3,2,1,1,4};
+
+        System.out.println(canJump(piles, 0, new HashMap<Integer, Boolean>()));
 
     }
 }
