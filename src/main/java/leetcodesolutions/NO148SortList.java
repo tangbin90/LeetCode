377de58250cae2ac1b7bebc2d0802ee49db6ee46
BinTang <tangbin90@users.noreply.github.com>
2025-03-24 01:27:27 +0800

    Add new alg

--- src/main/java/leetcodesolutions/NO148SortList.java
+++ src/main/java/leetcodesolutions/NO148SortList.java
@@ -2,8 +2,36 @@ package leetcodesolutions;
 
 import entity.ListNode;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 public class NO148SortList {
+
     public ListNode sortList(ListNode head) {
-        
+        if(head==null)
+            return head;
+        ArrayList<ListNode> list = new ArrayList<>();
+        ListNode cur = head;
+
+        while(cur != null){
+            list.add(cur);
+            cur = cur.next;
+        }
+
+        list.sort(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+
+        cur = list.get(0);
+        for(int i=1; i< list.size(); i++){
+            cur.next = list.get(i);
+            cur = list.get(i);
+        }
+        cur.next = null;
+
+        return list.get(0);
     }
 }
---
+++ src/main/java/leetcodesolutions/NO149MaxPointsOnALine.java
@@ -0,0 +1,9 @@
+package leetcodesolutions;
+
+public class NO149MaxPointsOnALine {
+    public int maxPoints(int[][] points) {
+        int res = 0;
+
+        return res;
+    }
+}
