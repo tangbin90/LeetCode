66635c9e13cabf6bcf4b0267b529adf00bd708f3
BinTang <tangbin90@users.noreply.github.com>
2025-03-27 01:08:43 +0800

    Add new alg

--- src/main/java/basic/BalanceTree.java
+++ src/main/java/basic/BalanceTree.java
@@ -1,4 +1,36 @@
 package basic;
-//二叉搜索树
+
+import entity.TreeNode;
+
+//二叉搜索树 search tree
 public class BalanceTree {
+    public static void plusone(TreeNode root){
+        if(root == null)
+            return;
+
+        root.val += 1;
+        plusone(root.left);
+        plusone(root.right);
+    }
+
+    public static boolean isSameTree(TreeNode root1, TreeNode root2){
+        if(root2 == null && root1 == null) return true;
+        if(root2 == null || root1 == null) return false;
+
+        if(root1.val !=root2.val )
+            return false;
+
+        return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right);
+    }
+
+    public static boolean isValidTree(TreeNode root, TreeNode min, TreeNode max){
+        if( root ==null)
+            return true;
+
+        if(min != null && min.val >= root.val) return false;
+        if(max != null && max.val <= root.val) return false;
+
+        return isValidTree(root.right, root, max) && isValidTree(root.left, min, root);
+
+    }
 }
--- src/main/java/basic/BinarySearch.java
+++ src/main/java/basic/BinarySearch.java
@@ -1,5 +1,64 @@
 package basic;
 
 public class BinarySearch {
+    //Binary search left
+    public static int find(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]< target){
+                low = mid +1;
+            }else if(nums[mid] > target){
+                high = mid -1;
+            } else {
+                return mid;
+            }
 
+        }
+        return -1;
+    }
+
+    public static int findLeft(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]< target){
+                low = mid +1;
+            }else if(nums[mid] > target){
+                high = mid -1;
+            } else {
+                high = mid -1;
+            }
+
+        }
+        if(low >= nums.length)
+            return -1;
+        return low;
+    }
+
+    public static int findRight(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+        while(low <= high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]< target){
+                low = mid +1;
+            }else if(nums[mid] > target){
+                high = mid -1;
+            } else {
+                low =  mid +1;
+            }
+
+        }
+
+        return high;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,5,43,123,123};
+
+        System.out.println(findRight(nums, 123));
+    }
 }
